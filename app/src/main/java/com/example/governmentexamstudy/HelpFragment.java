7075ad57bb208ac19c97956215cd99451978c3b8
Arvind Kushwaha <52744786+arvindkushwaha30@users.noreply.github.com>
2020-05-04 17:18:34 +0530

    home

--- app/src/main/java/com/example/governmentexamstudy/HelpFragment.java
+++ app/src/main/java/com/example/governmentexamstudy/HelpFragment.java
@@ -3,10 +3,36 @@ package com.example.governmentexamstudy;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -17,12 +43,175 @@ public class HelpFragment extends Fragment {
     public HelpFragment() {
         // Required empty public constructor
     }
-
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    ImageButton btnSpeak;
+    TextView txtSpeechInput, outputText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View view =inflater.inflate(R.layout.fragment_help, container, false);
+
+        btnSpeak = (ImageButton)view.findViewById(R.id.btnSpeak);
+        txtSpeechInput = (TextView) view.findViewById(R.id.txtSpeechInput);
+        outputText = (TextView) view.findViewById(R.id.outputTex);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                promptSpeechInput();
+            }
+        });
+
+        return view;
+    }
+
+    /**
+     * Showing google speech input dialog
+     */
+    private void promptSpeechInput() {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Say Something");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getActivity(),
+                    "orry! Your device doesn\\'t support speech input",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String userQuery=result.get(0);
+                    txtSpeechInput.setText(userQuery);
+                    RetrieveFeedTask task=new RetrieveFeedTask();
+                    task.execute(userQuery);
+
+                }
+                break;
+            }
+
+        }
+    }
+
+    // Create GetText Metod
+    public String GetText(String query) throws UnsupportedEncodingException {
+
+        String text = "";
+        BufferedReader reader = null;
+
+        // Send data
+        try {
+            // Defined URL  where to send data
+            URL url = new URL("https://api.api.ai/v1/query?v=20150910");
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            conn.setRequestProperty("Authorization", "Bearer 60f4cd2354ed490596fbae71a8862d23");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            //Create JSONObject here
+            JSONObject jsonParam = new JSONObject();
+            JSONArray queryArray = new JSONArray();
+            queryArray.put(query);
+            jsonParam.put("query", queryArray);
+//            jsonParam.put("name", "order a medium pizza");
+            jsonParam.put("lang", "en");
+            jsonParam.put("sessionId", "1234567890");
+
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            Log.d("karma", "after conversion is " + jsonParam.toString());
+            wr.write(jsonParam.toString());
+            wr.flush();
+            Log.d("karma", "json is " + jsonParam);
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+
+            // Read Server Response
+            while ((line = reader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+
+
+
+            JSONObject object1 = new JSONObject(text);
+            JSONObject object = object1.getJSONObject("result");
+            JSONObject fulfillment = null;
+            String speech = null;
+//            if (object.has("fulfillment")) {
+            fulfillment = object.getJSONObject("fulfillment");
+//                if (fulfillment.has("speech")) {
+            speech = fulfillment.optString("speech");
+//                }
+//            }
+
+
+            Log.d("karma ", "response is " + text);
+            return speech;
+
+        } catch (Exception ex) {
+            Log.d("karma", "exception at last " + ex);
+        } finally {
+            try {
+
+                reader.close();
+            } catch (Exception ex) {
+            }
+        }
+
+        return null;
+    }
+
+
+    class RetrieveFeedTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... voids) {
+            String s = null;
+            try {
+                s = GetText(voids[0]);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                Log.d("karma", "Exception occurred " + e);
+            }
+            return s;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            outputText.setText(s);
+            outputText.setMovementMethod(new ScrollingMovementMethod());
+        }
     }
 }
